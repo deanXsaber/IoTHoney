ead32a82720b1c811e780d5f9b4fd545697faf03
C.G <chongqi.guan@gmail.com>
2022-04-26 02:05:17 -0400

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting Server..."
-python3 ServerAWS.py '127.0.0.1' 8080  '127.0.0.1' 65523
+python3 ServerAWS.py --listen-host 127.0.0.1 --listen-port 8080 --connect-host 127.0.0.1 --connect-port 65523
